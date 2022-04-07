47d52621f047a61887171e69dd47425dbf095517
Neil_Kuan <guan840912@gmail.com>
2022-04-07 13:55:29 +0800

    chore: auto upgrade deps workflow

--- .projenrc.js
+++ .projenrc.js
@@ -1,8 +1,17 @@
-const { awscdk } = require("projen");
+const { awscdk } = require('projen');
 const project = new awscdk.AwsCdkTypeScriptApp({
-  cdkVersion: "2.19.0",
-  defaultReleaseBranch: "main",
-  name: "LambdaUrl",
+  cdkVersion: '2.19.0',
+  defaultReleaseBranch: 'main',
+  name: 'LambdaUrl',
   gitignore: ['cdk.out'],
+  depsUpgradeOptions: {
+    workflowOptions: {
+      labels: ['auto-approve'],
+    },
+  },
+  autoApproveOptions: {
+    secret: 'GITHUB_TOKEN',
+    allowedUsernames: ['neilkuan'],
+  },
 });
 project.synth();
